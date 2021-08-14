8043d71c8fc09def7e6fb14154fd35dcf76f6b77
Unknown <84217207+seaweedq12@users.noreply.github.com>
2021-08-15 09:51:02 +1000

    deloying page to heroku

---
+++ Develop/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/indexRouter.js');
+const { clog } = require('./middleware/clog');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(clog);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
